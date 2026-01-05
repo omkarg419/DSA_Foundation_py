1079ea97938447fd1c6ff594c616d1f6cd408ce4
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-05 16:23:57 +0530

    Implement insert and print methods for SinglyLinkedList class

--- singlyLL.py
+++ singlyLL.py
@@ -8,5 +8,28 @@ class SinglyLinkedList:
     def __init__(self,head=None):
         self.head=head
 
+    def insert_at_End(self,value):
+        temp= Node(value)
+        if (self.head != None):
+           t1=self.head
+           while(t1.next != None):
+                t1=t1.next
+           t1.next=temp 
+        else:
+            self.head=temp 
+
+    def printLL(self):
         
+        curr=self.head
+        while(curr.next != None):
+            print(curr.data)
+            curr=curr.next
+        print(curr.data)                 
+           
+obj = SinglyLinkedList()
+obj.insert_at_End(100)
+obj.insert_at_End(300)
+obj.insert_at_End(600)
+
+obj.printLL()
         
