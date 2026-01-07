fe357d45a9ae19de06ee99f3fb028ae2fdda0bc9
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-07 15:02:52 +0530

    Implement insertAtEnd method in doubly linked list

---
+++ doublyLL.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value=None):
+        self.prev=None
+        self.next=None
+        self.data=value
+
+class doublyLL:
+    def __init__(self,head=None):
+        self.head=head
+    
+    def insertAtEnd(self,value):
+        temp=Node(value)
+
+        if self.head == None:
+            self.head= temp
+            return
+        t=self.head
+        while t.next != None:
+            t.next=temp
+            temp.prev=t
+
+    def printLL(self):
+        curr=self.head
+        
+        
