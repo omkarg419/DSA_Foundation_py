a64d2ca2bc370db52c6cfd45dd161d3a5ee17b97
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-08 13:43:59 +0530

    Implement stack class with Push, Pop, Peek, and Length methods

---
+++ stack.py
@@ -0,0 +1,40 @@
+class stack:
+
+    def __init__(self):
+        self.s=[]
+
+    def Length(self):
+        return len(self.s)
+    
+    def Push(self,value):
+        # self.s.insert(0,value)
+        self.s.append(value)
+
+    def Peek(self):
+        if len(self.s) == 0:
+            raise Exception("Empty Stack")
+        else:
+            # return self.s[0]
+            return self.s[-1]
+    
+    def Pop(self):
+         if len(self.s) == 0:
+            raise Exception("Empty Stack")
+         else:
+            # return self.s.pop(0)
+            return self.s.pop()
+         
+
+stk =stack()
+
+stk.Push(10)
+stk.Push(20)
+stk.Push(30)
+stk.Push(40)
+stk.Push(50)
+
+print("peek element: ",stk.Peek())
+print("pop element: ",stk.Pop())
+print("pop element: ",stk.Pop())
+print("peek element: ",stk.Peek())
+print("Length of Stack: ",stk.Length())
