585bd3115848d90a3929f8f0140b004047b81db9
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-11 14:37:36 +0530

    Add insert and delete methods to Queue class

--- queue.py
+++ queue.py
@@ -5,6 +5,14 @@ class Queue:
     def isEmpty(self):
         return len(self.item) == 0
     
+    def insert(self,value):
+        self.item.append(value)
     
+    def delete(self):
+        if self.isEmpty() :
+            print("Queue is empty")
+        else:
+            return self.item.pop(0)    
+
 
         
