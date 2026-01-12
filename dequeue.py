5d917b2173c165bb98b414dc534e13ae46efc64c
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-12 16:08:07 +0530

    implement insertAtEnd and InsertATBeg for deque

--- dequeue.py
+++ dequeue.py
@@ -4,3 +4,9 @@ class Deque:
     
     def isEmpty(self):
         return len(self.item) == 0
+
+    def insertATEnd(self,value):
+        self.item.append(value)
+    
+    def insertAtBeg(self,value):
+        self.item.insert(0,value)
