a68ca9a0d588d843ff67ddbed195cf275ad845b9
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-12 16:27:28 +0530

    Fix isEmpty method calls in deleteATFornt and deleteAtReare methods

--- dequeue.py
+++ dequeue.py
@@ -12,13 +12,21 @@ class Deque:
         self.item.insert(0,value)
     
     def deleteATFornt(self):
-        if(self.isEmpty):
+        if(self.isEmpty()):
             print(" Deque is empty")
         else:
             return self.item.pop(0)
     
     def deleteAtReare(self):
-        if(self.isEmpty):
+        if(self.isEmpty()):
             print(" Deque is empty")
         else:
             return self.item.pop()
+        
+D=Deque()
+D.insertATEnd(10)
+D.insertATEnd(20)
+D.insertAtBeg(30)
+D.insertAtBeg(40)
+
+print(D.deleteATFornt())
