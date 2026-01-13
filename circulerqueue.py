cc0a665e99641b11e67df9c4d325f82dce1d7af4
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-13 14:50:03 +0530

    Refactor CirculerQueue class to improve enqueue method logic

--- circulerqueue.py
+++ circulerqueue.py
@@ -3,3 +3,18 @@ class CirculerQueue:
     def __init__(self,size):
         self.size= size
         self.item=[None]*size
+        self.front=self.rear=-1
+    
+    def enqueue(self,value):
+        if((self.rear + 1) % self.front):
+            print("Queue is Full")
+        elif self.front == -1:
+            self.front=self.rear=0
+            self.item[self.rear]=value
+        else:
+            self.rear=(self.rear +1) % self.size
+            self.item[self.rear]=value
+
+   
+
+
