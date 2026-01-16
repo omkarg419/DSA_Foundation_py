5ad30be29e9fca12c1cbcbdacaa5de34ccccc75d
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-16 11:59:58 +0530

    Fix preOrder function to correctly traverse the binary tree

--- treeTraversal.py
+++ treeTraversal.py
@@ -7,6 +7,8 @@ class Node:
 def preOrder(root):
     if root!= None:
         print("PreOrder: ",root.data, end=" -> ")
+        preOrder(root.left)
+        preOrder(root.right)
 
 root=Node(1)
 root.left=Node(3)
@@ -14,3 +16,5 @@ root.right=Node(5)
 root.left.left=Node(2)
 root.left.right=Node(4)
 root.right.right=Node(8)
+
+preOrder(root)
