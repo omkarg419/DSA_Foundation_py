a88549197db9b26e1f253992ebd572526a4d67ff
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-16 11:58:01 +0530

    Add initial implementation of binary tree with pre-order traversal

---
+++ treeTraversal.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.data=value
+
+def preOrder(root):
+    if root!= None:
+        print("PreOrder: ",root.data, end=" -> ")
+
+root=Node(1)
+root.left=Node(3)
+root.right=Node(5)
+root.left.left=Node(2)
+root.left.right=Node(4)
+root.right.right=Node(8)
