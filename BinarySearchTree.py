8c64526e1563c5df8011898ab65c4da912bfa1ce
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-17 11:51:37 +0530

    Add binary search tree implementation with Node class and insert function

---
+++ BinarySearchTree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.data=value
+
+def insert(root,value):
+    if root == None:
+        return Node(value)
+    if root.data == value:
+        return root
+    if root.data > value:
+        insert(root.left,value)
+    else:
+        insert(root.right,value)
+        
