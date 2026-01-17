d04bf60b7373cd03a9138b53988fd951d4a70b91
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-17 12:41:25 +0530

    Fix incorrect method call in insert function of BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -10,7 +10,7 @@ def insert(root,value):
     if root.data == value:
         return root
     if root.data > value:
-        root.left.insert(root.left,value)
+        root.left=insert(root.left,value)
     else:
         root.right=insert(root.right,value)
     return root
